bfa8753f2f4e55b2d16097161473cb9d1822348f
juan mesaglio <juan.mesaglio@gmail.com>
2021-10-31 21:24:58 -0300

    fix java error, disable github action

--- src/test/java/model/CartTest.java
+++ src/test/java/model/CartTest.java
@@ -1,55 +1,49 @@
 package test.java.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Locale;
+import javax.validation.Validator;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
-import java.util.Locale;
-
+import sample.entity.Item;
 import sample.model.Cart;
 import sample.model.CartItem;
-import sample.entity.Item;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ShoppingCartTest {
 
-	private Validator createValidator() {
-		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
-		localValidatorFactoryBean.afterPropertiesSet();
-		return localValidatorFactoryBean;
-    }
-    
-    @Test
-    public void addOneItem() {
-        LocaleContextHolder.setLocale(Locale.ENGLISH);
-        
-        Cart c = new Cart();
-        assertThat(c.getNumberOfItems()).isEqualTo(0);
-        assertThat(c.isEmpty()).isTrue();
+  private Validator createValidator() {
+    LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+    localValidatorFactoryBean.afterPropertiesSet();
+    return localValidatorFactoryBean;
+  }
 
-        Item i = new Item();
-        i.setItemId("1");
-        i.setProductId("1");
-        i.setQuantity(3);
-        i.setProductName("Producto 1");
+  @Test
+  public void addOneItem() {
+    LocaleContextHolder.setLocale(Locale.ENGLISH);
 
-        c.addItem(i, true);
-        assertThat(c.getNumberOfItems()).isEqualTo(1);
-        assertThat(c.isEmpty()).isFalse();
+    Cart c = new Cart();
+    assertThat(c.getNumberOfItems()).isEqualTo(0);
+    assertThat(c.isEmpty()).isTrue();
 
-        CartItem ci = c.getCartItem("1");
-        assertThat(ci).isNotNull();
-        assertThat(ci.getQuantity()).isEqualTo(1);
-        assertThat(ci.isInStock()).isTrue();
+    Item i = new Item();
+    i.setItemId("1");
+    i.setProductId("1");
+    i.setQuantity(3);
+    i.setProductName("Producto 1");
 
-        Item i1 = ci.getItem();
-        assertThat(i1.getQuantity()).isEqualTo(3);
-        assertThat(i1.getProductId()).isEqualTo("1");
+    c.addItem(i, true);
+    assertThat(c.getNumberOfItems()).isEqualTo(1);
+    assertThat(c.isEmpty()).isFalse();
 
-    }
+    CartItem ci = c.getCartItem("1");
+    assertThat(ci).isNotNull();
+    assertThat(ci.getQuantity()).isEqualTo(1);
+    assertThat(ci.isInStock()).isTrue();
 
+    Item i1 = ci.getItem();
+    assertThat(i1.getQuantity()).isEqualTo(3);
+    assertThat(i1.getProductId()).isEqualTo("1");
+  }
 }
